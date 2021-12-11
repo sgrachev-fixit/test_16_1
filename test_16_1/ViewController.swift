cc4ac0cbbfd8971d6d3471c088eb503e4a2b9c44
Moo <ornus.moo@gmail.com>
2021-12-11 15:46:10 +0300

    First commit for test_16_1

---
+++ test_16_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_16_1
+//
+//  Created by Sergey Grachev on 11.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
